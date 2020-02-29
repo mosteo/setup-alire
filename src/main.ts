7353c6d9abe4f8d8a8eabf8c14604f68ab4045ed
Alejandro R. Mosteo <amosteo@unizar.es>
2020-02-29 12:55:11 +0100

    Option for destination folder + fixes
    Cleanup
    
    Back to typescript
    
    Properly set env for MacOS
    
    Also, updated dependencies
    
    Debug log
    
    Plus updates
    
    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,30 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 const path = require("path");
 
-
-// This comes from https://github.com/actions/toolkit/blob/master/packages/tool-cache/src/tool-cache.ts
-// It would be nice to have an API to get the tempDirectory path.
-
-const IS_WINDOWS = process.platform === 'win32'
-const userAgent = 'actions/tool-cache'
-
-// On load grab temp directory and cache directory and remove them from env (currently don't want to expose this)
-let tempDirectory: string = process.env['RUNNER_TEMP'] || ''
-let cacheRoot: string = process.env['RUNNER_TOOL_CACHE'] || ''
-// If directories not found, place them in common temp locations
-if (!tempDirectory || !cacheRoot) {
-  let baseLocation: string
-  if (IS_WINDOWS) {
-    // On windows use the USERPROFILE env variable
-    baseLocation = process.env['USERPROFILE'] || 'C:\\'
-  } else {
-    if (process.platform === 'darwin') {
-      baseLocation = '/Users'
-    } else {
-      baseLocation = '/home'
-    }
-  }
-  if (!tempDirectory) {
-    tempDirectory = path.join(baseLocation, 'actions', 'temp')
-  }
-  if (!cacheRoot) {
-    cacheRoot = path.join(baseLocation, 'actions', 'cache')
-  }
-}
-
-const repo_url="https://github.com/alire-project/alire.git";
-const repo_branch="master";
-const alire_src=path.join(tempDirectory, "alire_src");
+const repo_url    : string = "https://github.com/alire-project/alire.git";
+const repo_branch : string = "master";
+const alire_src   : string = core.getInput('destination');
 
 async function run() {
-    try {
-        if (process.platform != 'linux') {
-            core.setFailed(`Alire is not available on ${process.platform} yet`);
-            return;
-        }
+   console.log("Starting setup-alire@exp v0.0.1...");
 
-        await exec.exec('sudo apt install gnat gprbuild');
-        await exec.exec(`git clone -b ${repo_branch} ${repo_url} ${alire_src}`);
-        process.chdir(alire_src);
-        await exec.exec(`git submodule update --init --recursive`);
-        await exec.exec(`gprbuild -j0 -p -XSELFBUILD=False -P alr_env.gpr`);
+   try {
+       await exec.exec(`git clone --recurse-submodules -b ${repo_branch} ${repo_url} ${alire_src}`);
 
-        core.addPath(path.join(process.cwd(), 'bin'));
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+       process.chdir(alire_src);
+
+       if (process.platform == "darwin") {
+           process.env.OS = "macOS"
+       }
+
+       await exec.exec(`gprbuild -j0 -p -XSELFBUILD=False -P alr_env.gpr -cargs -fPIC`);
+       
+       core.addPath(path.join(process.cwd(), 'bin'));
+   }
+   catch (error) {
+       core.setFailed(error.message);
+   }
 }
 
 run();
